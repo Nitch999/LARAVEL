33394d8c2251bc0577db071822a09acdfc93db1a
Nitch999 <dareoukpani99@.com>
2024-03-04 11:38:51 +0000

    Controller fonctionne

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class PagesController extends Controller
 {
     //
     public function home () {
         return view('welcome');
+    }
+    public function about() {
+        return view('jack');
+    }
+    public function conn () {
+        return view('dare');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+
 use Illuminate\Support\Facades\Route;
 
 
@@ -15,12 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','PagesController@home' );
+//Route::get('/','PagesController@home' );
+Route::get('/', [PagesController::class, 'home']);
 
-Route::get('/sit', function () {
-    return view('jack');
-});
 
-Route::get('/frm', function () {
-    return view('dare');
-});
+Route::get('/sit',[PagesController::class, 'about'] );
+
+Route::get('/frm',[PagesController::class, 'conn'] );
